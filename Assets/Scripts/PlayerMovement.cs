bc6c37dc92794c419a62dfd66cd738583da9e65b
Koval Vadym <vzlobinkov@gmail.com>
2023-03-22 18:32:21 +0200

    Add UI Joystick

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -13,10 +13,17 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private LayerMask _jumpableGround;
     [SerializeField] private float _moveSpeed = 7f;
     [SerializeField] private float _jumpForce = 14f;
+    [SerializeField] private Joystick _joystick;
 
     private enum MovementState { Idle, Running, Jumping, Falling }
 
     [SerializeField] private AudioSource _jumpSoundEffect;
+
+    public void Jump()
+    {
+        _jumpSoundEffect.Play();
+        _rigidBody.velocity = new Vector2(_rigidBody.velocity.x, _jumpForce);
+    }
 
     // Start is called before the first frame update
     private void Start()
@@ -30,48 +37,75 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
-        _xDir = Input.GetAxisRaw("Horizontal");
-        _rigidBody.velocity = new Vector2(_xDir * _moveSpeed, _rigidBody.velocity.y);
-
-        if (IsGrounded() && Input.GetButtonDown("Jump"))
+        if (_joystick.Horizontal == 0)
         {
-            _jumpSoundEffect.Play();
-            _rigidBody.velocity = new Vector2(_rigidBody.velocity.x, _jumpForce);
+            _xDir = Input.GetAxisRaw("Horizontal");
+            _rigidBody.velocity = new Vector2(_xDir * _moveSpeed, _rigidBody.velocity.y);
+        } else if (_joystick.Horizontal != 0)
+        {
+            _rigidBody.velocity = new Vector2(_joystick.Horizontal * _moveSpeed, _rigidBody.velocity.y);
         }
 
-        UpdateAnimationState();
+        if (IsGrounded() && (Input.GetButtonDown("Jump")))
+        {
+            Jump();
+        }
 
-        /*Debug.Log($"PLAYER Y: {rigidBody.transform.position.x}");*/
+        if (_joystick.Horizontal == 0)
+        {
+            UpdateAnimationState();
+        }
+        else if (_joystick.Horizontal != 0)
+        {
+            UpdateAnimationState(true);
+        }
     }
 
-    private void UpdateAnimationState()
+    private void UpdateAnimationState(bool forJoystick = false)
     {
         MovementState state;
+        if (forJoystick)
+        {
+            if (_joystick.Horizontal > 0f) // right
+            {
+                state = MovementState.Running;
+                _sprite.flipX = false;
+            }
+            else if (_joystick.Horizontal < 0f) // left
+            {
+                state = MovementState.Running;
+                _sprite.flipX = true;
+            }
+            else
+            {
+                state = MovementState.Idle;
+            }
+        } else
+        {
+            if (_xDir > 0f) // right
+            {
+                state = MovementState.Running;
+                _sprite.flipX = false;
+            }
+            else if (_xDir < 0f) // left
+            {
+                state = MovementState.Running;
+                _sprite.flipX = true;
+            }
+            else
+            {
+                state = MovementState.Idle;
+            }
 
-        if (_xDir > 0f) // right
-        {
-            state = MovementState.Running;
-            _sprite.flipX = false;
+            if (_rigidBody.velocity.y > .1f)
+            {
+                state = MovementState.Jumping;
+            }
+            else if (_rigidBody.velocity.y < -.1f)
+            {
+                state = MovementState.Falling;
+            }
         }
-        else if (_xDir < 0f) // left
-        {
-            state = MovementState.Running;
-            _sprite.flipX = true;
-        }
-        else
-        {
-            state = MovementState.Idle;
-        }
-
-        if (_rigidBody.velocity.y > .1f)
-        {
-            state = MovementState.Jumping;
-        }
-        else if (_rigidBody.velocity.y < -.1f)
-        {
-            state = MovementState.Falling;
-        }
-
         _animator.SetInteger("state", (int)state);
     }
 
